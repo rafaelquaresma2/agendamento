1386098005d8dc9f62ed6347b3e5156540c72b7a
unknown <Administrador@HSMBRL0109.HSM.INT>
2022-06-03 16:46:42 -0300

    agendamento 2

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const { database } = require("pg/lib/defaults");
+
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    database: 'sistema',
+    define:{
+        timestamps: true,
+        undercored: true,
+        undercoredAll: true,
+    }
+}
